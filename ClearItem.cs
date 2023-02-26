749d9a7d15b509934f41cab65dd67e41e73fcc50
dh3b <ozootly@gmail.com>
2023-02-26 11:52:24 +0100

    Upload

---
+++ ClearItem.cs
@@ -0,0 +1,36 @@
+﻿using Exiled.API.Features;
+using Exiled.API.Enums;
+using System;
+
+namespace clearItem
+{
+    public class ClearItem : Plugin<Config>
+    {
+        public override string Name => "Clear Item";
+        public override string Author => "dheb";
+
+        public static ClearItem Instance { get; private set; }
+        public override PluginPriority Priority { get; } = PluginPriority.Medium;
+        public override Version RequiredExiledVersion => new Version(6, 0, 0);
+        public override Version Version => new Version(1, 0, 0);
+
+        public override void OnEnabled()
+        {
+            Log.Info("Clear Item plugin has been successfully enabled!");
+            Instance = this;
+            RegisterEvents();
+            base.OnEnabled();
+        }
+
+        public override void OnDisabled()
+        {
+            Log.Info("Clear Item plugin has been disabled.");
+            Instance = null;
+            UnregisterEvents();
+            base.OnDisabled();
+        }
+
+        private void RegisterEvents() {}
+        private void UnregisterEvents() {}
+    }
+}
---
+++ Commands/ClearCommand.cs
@@ -0,0 +1,39 @@
+﻿using CommandSystem;
+using System;
+using Exiled.API.Features;
+
+namespace clearItem.Commands
+{
+    [CommandHandler(typeof(ClientCommandHandler))]
+    class ClearCommand : ICommand
+    {
+        public string Command { get; } = "clearItem";
+        public string[] Aliases { get; } = { "secureItem" };
+        public string Description { get; } = "Usuń Przedmiot, który jest obecnie trzymany w ręce.";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            Player player = Player.Get(sender);
+
+            if (sender is null)
+            {
+                response = "Aby użyć tej komendy musisz być graczem!";
+                return false;
+            }
+            else
+            {
+                if (player.CurrentItem != null)
+                { 
+                    player.RemoveHeldItem(true);
+                    response = "Przedmiot został usunięty.";
+                    return true;
+                }
+                else
+                {
+                    response = "Aby użyć tej komendy musisz trzymać przedmiot w ręce!";
+                    return false;
+                }
+            }
+        }
+    }
+}
